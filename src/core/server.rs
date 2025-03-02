071d4b19da7281f6042c2da88e95bed12661aa14
SanitoGonzalez <ddalkisoup23@gmail.com>
2025-03-02 16:11:49 +0900

    Organize server code

--- src/core/server.rs
+++ src/core/server.rs
@@ -4,27 +4,40 @@ use std::error::Error;
 use std::net::SocketAddr;
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::{broadcast, mpsc};
+use tokio::task::JoinSet;
 
 pub async fn run_server(listen_port: u16) -> Result<(), Box<dyn Error>> {
-    let listen_addr = SocketAddr::from(([0, 0, 0, 0], listen_port));
-    let listener = TcpListener::bind(listen_addr).await?;
-
-    let (_shutdown_tx, mut shutdown_rx) = broadcast::channel(1);
-
-    println!("Listening on {}", listener.local_addr()?);
+    let (_shutdown_tx, shutdown_rx) = broadcast::channel(1);
 
     let mut waiting_room = Room::new(0, shutdown_rx.resubscribe());
     let in_message_tx = waiting_room.in_message_tx.clone();
 
-    tokio::spawn(async move {
+    let mut tasks = JoinSet::new();
+    tasks.spawn(async move {
+        listen(listen_port, in_message_tx, shutdown_rx).await;
+    });
+    tasks.spawn(async move {
         waiting_room.run().await;
     });
+
+    while let Some(_) = tasks.join_next().await {}
+    Ok(())
+}
+
+async fn listen(
+    listen_port: u16,
+    in_message_tx: mpsc::Sender<Vec<u8>>,
+    mut shutdown_rx: broadcast::Receiver<()>,
+) {
+    let listen_addr = SocketAddr::from(([0, 0, 0, 0], listen_port));
+    let listener = TcpListener::bind(listen_addr).await.unwrap();
+    println!("Server listening at {}", listen_addr);
 
     loop {
         tokio::select! {
             result = listener.accept() => match result {
                 Ok((socket, _)) => {
-                    on_accept(socket, in_message_tx.clone(), shutdown_rx.resubscribe());
+                    accept(socket, in_message_tx.clone(), shutdown_rx.resubscribe());
                 },
                 Err(e) => {
                     eprintln!("Error accepting connection: {}", e);
@@ -36,12 +49,9 @@ pub async fn run_server(listen_port: u16) -> Result<(), Box<dyn Error>> {
             }
         }
     }
-
-    Ok(())
 }
 
-
-fn on_accept(
+fn accept(
     stream: TcpStream,
     in_message_tx: mpsc::Sender<Vec<u8>>,
     shutdown_rx: broadcast::Receiver<()>,
