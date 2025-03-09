82e9b9a20967f6c83d08bc9f9b4db5a7b8fd2978
Sanito Gonzalez <ddalkisoup23@gmail.com>
2025-03-09 22:40:45 +0900

    fix game room update with interval timer

--- src/rooms/game_room.rs
+++ src/rooms/game_room.rs
@@ -1,31 +1,46 @@
 use bevy_ecs::prelude::*;
 use tokio::sync::{broadcast, mpsc};
+use tokio::time::{self, Duration, Instant};
+use crate::core::session::InMessage;
 
-pub async fn run_room(
-    mut in_message_rx: mpsc::Receiver<Vec<u8>>,
+pub async fn run(
+    mut in_message_rx: mpsc::Receiver<InMessage>,
     mut shutdown_rx: broadcast::Receiver<()>,
 ) {
     let mut world = World::default();
+    let mut interval = time::interval(Duration::from_millis(100));
+    let mut last_tick = Instant::now();;
+
+    let mut message_buffer = Vec::with_capacity(64);
 
     loop {
         tokio::select! {
-            _ = update(&mut world) => {}
-            result = in_message_rx.recv() => match result {
-                Some(message) => {
-                    println!("Received {} bytes of message", message.len());
-                }
-                None => { break; }
+            _ = interval.tick() => {
+                let now = Instant::now();
+                update(&mut world, now - last_tick);
+                last_tick = now;
             },
-            _ = shutdown_rx.recv() => { break; },
+            n = in_message_rx.recv_many(&mut message_buffer, 64) => {
+                if n == 0 {
+                    break;
+                }
+
+                for message in message_buffer.drain(0..n) {
+                    handle(message);
+                }
+            },
+            _ = shutdown_rx.recv() => break,
         }
     }
 }
 
-async fn update(world: &mut World) {
-    println!("Room update");
-    let mut schedule = Schedule::default();
+fn update(world: &mut World, dt: Duration) {
+    println!("Room update; dt={}", dt.as_secs());
+    // let mut schedule = Schedule::new();
     //TODO: Add systems
-    // schedule.run(&mut world);
+    // schedule.run(world);
+}
 
-    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+fn handle(message: InMessage) {
+    let (session_ctx, data) = message;
 }
